0ef3e11367ab67ba2f0e6bfba7ca392de0715a07
eLizabethWanjirU1 <107485821+eLizabethWanjirU1@users.noreply.github.com>
2024-06-06 10:24:53 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Services from './components/Services';
+import Resume from './components/Resume';
+import Projects from './components/Projects';
+import Blog from './components/Blog';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/services" component={Services} />
+      <Route path="/resume" component={Resume} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/blog" component={Blog} />
+      <Route path="/contact" component={Contact} />
+    </Switch>
+  </Router>
+);
 
 export default App;
